ed360788f0ad5f2530d7eca9fe72d7e678df4dfc
Casey Link <unnamedrambler@gmail.com>
2025-09-11 19:05:10 +0200

    implement createtables for html formats

--- scripts/createtables.clj
+++ scripts/createtables.clj
@@ -48,18 +48,23 @@
                (inc count))))))
 
 (defn determine-rules
-  "Determine PRECIS rules that apply to a codepoint (simplified version)"
-  [_unicode-data _derived-props _cp prop]
-  ;; This is a simplified version - a full implementation would need
-  ;; to track which specific RFC 8264 Section 9 rules were applied
-  (case prop
-    :pvalid     "AE"  ; Simplified - could be A, E, or both
-    :free-pval  "O" ; Simplified
-    :disallowed ""
-    :unassigned ""
-    :contextj   "H"
-    :contexto   "B"
-    ""))
+  "Determine PRECIS rules that apply to a codepoint based on RFC 8264 Section 9"
+  [unicode-data derived-props cp]
+  (let [assigned? (contains? unicode-data cp)]
+    (cond
+      (not assigned?)                               ""
+      (common/ascii7? cp)                           "K"
+      (common/join-control? cp)                     "H"
+      (common/old-hangul-jamo? cp)                  "I"
+      (common/precis-ignorable? derived-props cp)   "M"
+      (common/control? unicode-data cp)             "L"
+      (common/has-compat? unicode-data cp)          "F"
+      (common/letter-digits? unicode-data cp)       "A"
+      (common/other-letter-digits? unicode-data cp) "B"
+      (common/spaces? unicode-data cp)              "N"
+      (common/symbols? unicode-data cp)             "O"
+      (common/punctuation? unicode-data cp)         "P"
+      :else                                         "")))
 
 (defn write-allcodepoints-txt
   "Write allcodepoints.txt format: <codepoint>;<property>;<rules>;<name>"
@@ -69,122 +74,168 @@
 
     (with-open [writer (io/writer output-file)]
       (doseq [cp all-codepoints]
-        (let [prop     (get properties cp :unassigned)
-              prop-str (get common/precis-properties prop (str/upper-case (name prop)))
-              rules    (determine-rules unicode-data derived-props cp prop)
-              name     (common/get-character-name unicode-data cp)]
+        (let [prop  (get properties cp :unassigned)
+              rules (determine-rules unicode-data derived-props cp)
+              name  (common/get-character-name unicode-data cp)]
           (.write writer (format "%04X;%s;%s;%s\n"
-                                 cp prop-str rules name)))))))
+                                 cp (common/precis-properties prop) rules name)))))))
+
+(defn load-scripts-data
+  "Load Unicode Scripts.txt data"
+  [version-dir]
+  (let [scripts-file (str version-dir "/Scripts.txt")]
+    (when (.exists (io/file scripts-file))
+      (with-open [reader (io/reader scripts-file)]
+        (->> (line-seq reader)
+             (remove #(or (str/blank? %) (str/starts-with? % "#")))
+             (map (fn [line]
+                    (let [[range-part script-part] (str/split line #"\s*;\s*" 2)
+                          script                   (str/trim (first (str/split script-part #"\s*#")))]
+                      (if (str/includes? range-part "..")
+                        (let [[start-str end-str] (str/split range-part #"\.\.")]
+                          {:start  (Integer/parseInt start-str 16)
+                           :end    (Integer/parseInt end-str 16)
+                           :script script})
+                        {:start  (Integer/parseInt range-part 16)
+                         :end    (Integer/parseInt range-part 16)
+                         :script script}))))
+             (reduce (fn [acc {:keys [start end script]}]
+                       (reduce #(assoc %1 %2 script) acc (range start (inc end))))
+                     {}))))))
+
+(defn get-script
+  "Get script for a codepoint"
+  [scripts-data cp]
+  (get scripts-data cp "Common"))
+
+(defn html-escape-name
+  "Escape character name for HTML display, using <control> for control characters"
+  [unicode-data cp]
+  (let [raw-name     (common/get-character-name unicode-data cp)
+        display-name (if (= (get-in unicode-data [cp :name]) "<control>")
+                       "<control>"
+                       raw-name)]
+    (-> display-name
+        (str/replace "&" "&amp;")
+        (str/replace "<" "&lt;")
+        (str/replace ">" "&gt;"))))
+
+(defn write-html-row
+  "Write a single HTML table row for a codepoint"
+  [writer unicode-data derived-props properties cp]
+  (let [prop         (get properties cp :unassigned)
+        rules        (determine-rules unicode-data derived-props cp)
+        gc           (get-in unicode-data [cp :general-category] "Cn")
+        name         (html-escape-name unicode-data cp)
+        char-display (format "&#x%04X;" cp)]
+    (.write writer (format "<TR><TD>U+%04X</TD><TD>%s</TD><TD>%s</TD><TD>%s</TD><TD>%s</TD><TD>%s</TD></TR>\n\n"
+                           cp char-display (common/precis-properties prop) rules gc name))))
+
+(defn write-html-group
+  "Write HTML table for a group of codepoints"
+  [writer group-name codepoints unicode-data derived-props properties]
+  (.write writer (format "<H3>%s</H3>\n" group-name))
+  (.write writer "<TABLE border=1><TR><TH>Code(s)</TH><TH>Char</TD><TH>U-label</TH><TH>Rules</TH><TH>GC</TD><TH>Name(s)</TH></TR>\n")
+  (doseq [cp (sort codepoints)]
+    (write-html-row writer unicode-data derived-props properties cp))
+  (.write writer "</TABLE>\n\n"))
 
 (defn write-byscript-html
-  "Write HTML table sorted by Unicode script (placeholder implementation)"
-  [output-dir _version unicode-data derived-props properties]
-  (let [output-file (str output-dir "/byscript.html")]
-    (println "  Writing byscript.html (grouped by scripts)")
+  "Write HTML table sorted by Unicode script"
+  [output-dir version unicode-data derived-props properties]
+  (let [output-file   (str output-dir "/byscript.html")
+        version-dir   (str "data/" version)
+        scripts-data  (load-scripts-data version-dir)
+        script-groups (->> (keys unicode-data)
+                           (group-by #(get-script scripts-data %))
+                           (into (sorted-map)))]
+
+    (println (format "  Writing byscript.html (grouped by %d scripts)" (count script-groups)))
 
     (with-open [writer (io/writer output-file)]
-      (.write writer "<HTML><HEAD><TITLE>PRECIS by Script</TITLE></HEAD><BODY>\n")
-      (.write writer "<TABLE border=1>\n")
-      (.write writer "<TR><TH>Code(s)</TH><TH>Char</TH><TH>U-label</TH><TH>Rules</TH><TH>GC</TH><TH>Name(s)</TH></TR>\n")
-
-      ;; Simple implementation - just output first 100 codepoints as example
-      (doseq [cp (take 100 all-codepoints)]
-        (let [prop         (get properties cp :unassigned)
-              prop-str     (get common/precis-properties prop (str/upper-case (name prop)))
-              rules        (determine-rules unicode-data derived-props cp prop)
-              gc           (get-in unicode-data [cp :general-category] "Cn")
-              name         (common/get-character-name unicode-data cp)
-              char-display (if (<= 0x20 cp 0x7E)
-                             (format "&#x%04X;" cp)
-                             "&nbsp;")]
-          (.write writer (format "<TR><TD>U+%04X</TD><TD>%s</TD><TD>%s</TD><TD>%s</TD><TD>%s</TD><TD>%s</TD></TR>\n"
-                                 cp char-display prop-str rules gc name))))
-
-      (.write writer "</TABLE>\n</BODY></HTML>\n"))))
+      (.write writer "<HTML>\n<BODY>\n<H3>Codepoints by script</H3>\n")
+      (doseq [[script codepoints] script-groups]
+        (write-html-group writer script codepoints unicode-data derived-props properties))
+      (.write writer "</BODY></HTML>\n"))))
 
 (defn write-bygc-html
-  "Write HTML table sorted by General Category (placeholder implementation)"
+  "Write HTML table sorted by General Category"
   [output-dir _version unicode-data derived-props properties]
-  (let [output-file (str output-dir "/bygc.html")]
-    (println "  Writing bygc.html (grouped by general categories)")
+  (let [output-file (str output-dir "/bygc.html")
+        gc-groups   (->> (keys unicode-data)
+                         (group-by #(get-in unicode-data [% :general-category] "Cn"))
+                         (into (sorted-map)))]
+
+    (println (format "  Writing bygc.html (grouped by %d general categories)" (count gc-groups)))
 
     (with-open [writer (io/writer output-file)]
-      (.write writer "<HTML><HEAD><TITLE>PRECIS by General Category</TITLE></HEAD><BODY>\n")
-      (.write writer "<TABLE border=1>\n")
-      (.write writer "<TR><TH>Code(s)</TH><TH>Char</TH><TH>U-label</TH><TH>Rules</TH><TH>GC</TH><TH>Name(s)</TH></TR>\n")
+      (.write writer "<HTML>\n<BODY>\n<H3>Codepoints by GeneralCategory</H3>\n")
+      (doseq [[gc codepoints] gc-groups]
+        (write-html-group writer gc codepoints unicode-data derived-props properties))
+      (.write writer "</BODY></HTML>\n"))))
 
-      ;; Simple implementation - just output first 100 codepoints as example
-      (doseq [cp (take 100 all-codepoints)]
-        (let [prop         (get properties cp :unassigned)
-              prop-str     (get common/precis-properties prop (str/upper-case (name prop)))
-              rules        (determine-rules unicode-data derived-props cp prop)
-              gc           (get-in unicode-data [cp :general-category] "Cn")
-              name         (common/get-character-name unicode-data cp)
-              char-display (if (<= 0x20 cp 0x7E)
-                             (format "&#x%04X;" cp)
-                             "&nbsp;")]
-          (.write writer (format "<TR><TD>U+%04X</TD><TD>%s</TD><TD>%s</TD><TD>%s</TD><TD>%s</TD><TD>%s</TD></TR>\n"
-                                 cp char-display prop-str rules gc name))))
+(defn xml-escape
+  "Escape text for XML with optional quote escaping"
+  ([text] (xml-escape text false))
+  ([text escape-quotes?]
+   (cond-> text
+     true           (str/replace "&" "&amp;")
+     true           (str/replace "<" "&lt;")
+     true           (str/replace ">" "&gt;")
+     escape-quotes? (str/replace "\"" "&quot;"))))
 
-      (.write writer "</TABLE>\n</BODY></HTML>\n"))))
+(defn format-range
+  "Format codepoint range with specified separator"
+  [start end separator]
+  (if (= start end)
+    (format "%04X" start)
+    (format "%04X%s%04X" start separator end)))
+
+(defn write-with-progress
+  "Execute function with file writer and progress message"
+  [output-file format-msg count-val write-fn]
+  (println (format format-msg count-val))
+  (with-open [writer (io/writer output-file)]
+    (write-fn writer)))
 
 (defn write-xmlrfc-xml
   "Write UCD-style range notation format with XML wrapper elements"
   [output-dir _version unicode-data properties]
   (let [output-file (str output-dir "/xmlrfc.xml")
         ranges      (common/compress-ranges properties)]
-    (println (format "  Writing xmlrfc.xml (%,d ranges)" (count ranges)))
+    (write-with-progress output-file "  Writing xmlrfc.xml (%,d ranges)" (count ranges)
+                         (fn [writer]
+                           (.write writer "<section title=\"Codepoints in Unicode Character Database (UCD) format\">\n")
+                           (.write writer "<figure><artwork>\n")
 
-    (with-open [writer (io/writer output-file)]
-      ;; XML wrapper elements
-      (.write writer "<section title=\"Codepoints in Unicode Character Database (UCD) format\">\n")
-      (.write writer "<figure><artwork>\n")
+                           (doseq [[start end prop] ranges]
+                             (let [range-str (format-range start end "..")
+                                   comment   (xml-escape (common/ucd-range-description unicode-data start end))
+                                   line      (format "%-12s; %-11s # %s" range-str (common/precis-properties prop) comment)]
+                               (.write writer (str line "\n"))))
 
-      (doseq [[start end prop] ranges]
-        (let [prop-str  (get common/precis-properties prop (str/upper-case (name prop)))
-              range-str (if (= start end)
-                          (format "%04X" start)
-                          (format "%04X..%04X" start end))
-              comment   (-> (common/ucd-range-description unicode-data start end)
-                             ;; XML entity escaping
-                            (str/replace "&" "&amp;")
-                            (str/replace "<" "&lt;")
-                            (str/replace ">" "&gt;"))
-              ;; Format with proper alignment
-              line      (format "%-12s; %-11s # %s" range-str prop-str comment)]
-          (.write writer (str line "\n"))))
-
-      ;; Close XML wrapper elements
-      (.write writer "</artwork></figure></section>\n"))))
+                           (.write writer "</artwork></figure></section>\n")))))
 
 (defn write-iana-xml
   "Write IANA XML registry format"
   [output-dir _version unicode-data properties]
   (let [output-file (str output-dir "/idnabis-tables.xml")
         ranges      (common/compress-ranges properties)]
-    (println (format "  Writing idnabis-tables.xml (%,d records)" (count ranges)))
+    (write-with-progress output-file "  Writing idnabis-tables.xml (%,d records)" (count ranges)
+                         (fn [writer]
+                           (.write writer "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+                           (.write writer "<registry xmlns=\"http://www.iana.org/assignments\" id=\"precis-tables\">\n")
 
-    (with-open [writer (io/writer output-file)]
-      (.write writer "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-      (.write writer "<registry xmlns=\"http://www.iana.org/assignments\" id=\"precis-tables\">\n")
+                           (doseq [[start end prop] ranges]
+                             (let [codepoint-str (format-range start end "-")
+                                   description   (xml-escape (common/iana-range-description unicode-data start end) true)]
+                               (.write writer "  <record>")
+                               (.write writer (format "<codepoint>%s</codepoint>" codepoint-str))
+                               (.write writer (format "<property>%s</property>" (common/precis-properties prop)))
+                               (.write writer (format "<description>%s</description>" description))
+                               (.write writer "</record>\n")))
 
-      (doseq [[start end prop] ranges]
-        (let [prop-str      (get common/precis-properties prop (str/upper-case (name prop)))
-              codepoint-str (if (= start end)
-                              (format "%04X" start)
-                              (format "%04X-%04X" start end))
-              description   (-> (common/iana-range-description unicode-data start end)
-                                (str/replace "&" "&amp;")
-                                (str/replace "<" "&lt;")
-                                (str/replace ">" "&gt;")
-                                (str/replace "\"" "&quot;"))]
-          (.write writer "  <record>")
-          (.write writer (format "<codepoint>%s</codepoint>" codepoint-str))
-          (.write writer (format "<property>%s</property>" prop-str))
-          (.write writer (format "<description>%s</description>" description))
-          (.write writer "</record>\n")))
-
-      (.write writer "</registry>\n"))))
+                           (.write writer "</registry>\n")))))
 
 (defn generate-for-version
   "Generate all output formats for a specific Unicode version"
